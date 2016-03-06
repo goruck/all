30febfc8cee5879882875a4fa90d3b74d1d002dc
goruck <lindo.st.angel@gmail.com>
2016-03-06 10:00:26 -0800

    fixed system ready falsing problem and cleaned up code

--- lambda/all.js
+++ lambda/all.js
@@ -143,125 +143,6 @@ function getWelcomeResponse(callback) {
 }
 
 /*
- * Mapping from alarm zones to friendly place names.
- */
-var places = {
-    "Zone1": {
-        "1":  "front door",
-	"2":  "living room left window",
-	"3":  "living room right window",
-	"4":  "dining room left window",
-	"5":  "dining room center left window",
-	"6":  "dining room center right window",
-	"7":  "dining room right window",
-	"8":  "kitchen left window"
-    },
-    "Zone2": {
-        "1":  "kitchen right window",
-	"2":  "breakfast nook left window",
-	"3":  "breakfast nook side right window",
-	"4":  "breakfast nook left window",
-	"5":  "breakfast nook center left window",
-	"6":  "breakfast nook center right window",
-	"7":  "breakfast nook right window",
-	"8":  "family room slider door"
-    },
-    "Zone3": {
-        "1":  "family room left window",
-	"2":  "family room center window",
-	"3":  "family room right window",
-        "4":  "guest bedroom left window",
-	"5":  "guest bedroom center left window",
-	"6":  "guest bedroom center right window",
-	"7":  "guest bedroom right window",
-	"8":  "first floor bathroom window"
-    },
-    "Zone4": {
-        "1":  "front landing window",
-	"2":  "upstairs slider",
-	"3":  "front motion",
-	"4":  "hall motion",
-	"5":  "upstairs motion",
-	"6":  "playroom motion",
-	"7":  "playroom window",
-	"8":  "playroom door"
-    },
-};
-
-function zoneToPlace(zone, sensor) {
-    return places[zone][sensor];
-}
-
-/*
- * Find friendly names of places not ready to be armed.
- */
-function findPlacesNotReady(panelStatus) {
-    var zoneRegex = /(Zone\d \d{1,2})((, \d{1,2}){1,8})?/g;
-    var zonesNotReady = panelStatus.match(zoneRegex); // array with zones not ready
-    var placesNotReady = "";
-    for(i = 0; i < zonesNotReady.length; i++) {
-        var zoneNotReady = zonesNotReady[i].slice(0,5);
-        var sensorRegex = /\d{1,2}/g;
-        var sensorsNotReady = zonesNotReady[i].slice(6).match(sensorRegex); // array with sensors in zone not ready
-        for(j = 0; j < sensorsNotReady.length; j++) {
-            var sensorNotReady = sensorsNotReady[j];
-	    placesNotReady += zoneToPlace(zoneNotReady, sensorNotReady) +',';
-        }
-    }
-    return placesNotReady;
-}
-
-/*
- * Gets the panel status to be used in the intent handlers.
- */
-function getPanelStatus (callback) {
-
-    var tls = require('tls'),
-        fs = require('fs'),
-        PORT = fs.readFileSync('port.txt').toString("utf-8", 0, 5),
-        HOST = fs.readFileSync('host.txt').toString("utf-8", 0, 14),
-        CERT = fs.readFileSync('client.crt'),
-        KEY  = fs.readFileSync('client.key'),
-        CA   = fs.readFileSync('ca.crt'),
-        panelStatus = "";
-
-    var options = {
-        host: HOST,
-        port: PORT,
-        cert: CERT,
-        key: KEY,
-        ca: CA,
-        rejectUnauthorized: true
-    };
-
-    var socket = tls.connect(options, function() {
-        console.log('getPanelStatus socket connected to ' +HOST);
-        if(socket.authorized){
-            console.log('host is authorized');
-        }
-        else{
-            console.log('host cert auth error: ', socket.authorizationError);
-        }
-        socket.write('idle' +'\n');
-    });
-
-    socket.on('data', function(data) {
-        panelStatus += data.toString();
-    });
-	
-    socket.on('close', function () {
-	console.log('getPanelStatus socket disconnected from host ' +HOST);
-	callback(panelStatus);
-    });
-	
-    socket.on('error', function(ex) {
-	console.log("handled getPanelStatus socket error");
-	console.log(ex);
-    });
-
-}
-
-/*
  * Gets the panel keypress from the user in the session.
  * Check to make sure keypress is valid.
  * Prepares the speech to reply to the user.
@@ -274,26 +155,8 @@ function sendKeyInSession(panelStatus, intent, session, callback) {
     var repromptText = "";
     var shouldEndSession = true; // end session after sending keypress
     var speechOutput = "";
-    var tls = require('tls');
-    var fs = require('fs');
-    var PORT = fs.readFileSync('port.txt').toString("utf-8", 0, 5);
-    var HOST = fs.readFileSync('host.txt').toString("utf-8", 0, 14);
-    var CERT = fs.readFileSync('client.crt');
-    var KEY  = fs.readFileSync('client.key');
-    var CA   = fs.readFileSync('ca.crt');
-    var options = {
-        host: HOST,
-        port: PORT,
-        cert: CERT,
-        key: KEY,
-        ca: CA,
-        rejectUnauthorized: true
-    };
 
-    var isReady = panelStatus.indexOf('LED Status Ready') > -1; // true if system is ready
-    var isArmed = panelStatus.indexOf('Armed') > -1; // true if system is armed
-    var ValidValues = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9',
-                       'stay', 'away', 'star', 'pound'];
+    var ValidValues = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'stay', 'away', 'star', 'pound'];
     var num = KeysSlot.value;
     var isValidValue = ValidValues.indexOf(num) > -1; // true if a valid value was passed
     
@@ -305,23 +168,18 @@ function sendKeyInSession(panelStatus, intent, session, callback) {
             callback(sessionAttributes,
                      buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
         } else {
-            if ((num === 'stay' || num === 'away') && isArmed) {
+            if ((num === 'stay' || num === 'away') && isArmed(panelStatus)) {
                 speechOutput = "System is already armed,";
                 callback(sessionAttributes,
                          buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
-            } else if ((num === 'stay' || num === 'away') && !isReady) {
+            } else if ((num === 'stay' || num === 'away') && !zonesNotActive(panelStatus)) {
                 var placesNotReady = findPlacesNotReady(panelStatus); // get friendly names of zones not ready
                 speechOutput = "System cannot be armed, because these zones are not ready," +placesNotReady;
                 callback(sessionAttributes,
                          buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
             } else {
-                var socket = tls.connect(options, function() {
-                    console.log('sendKeyInSession socket connected to host ' +HOST);
-                    if(socket.authorized){
-                        console.log('host is authorized');
-                    } else {
-                        console.log('host cert auth error: ', socket.authorizationError);
-                    }
+                var socket = tls.connect(socketOptions, function() {
+                    console.log('sendKeyInSession socket connected to host: ' +HOST);
                     socket.write(num +'\n');
                     console.log('wrote ' +num);
                 });
@@ -332,12 +190,11 @@ function sendKeyInSession(panelStatus, intent, session, callback) {
                 });
 
                 socket.on('close', function() { // wait for FIN packet from server
-                    console.log('sendKeyInSession socket disconnected from host ' +HOST);
+                    console.log('sendKeyInSession socket disconnected from host: ' +HOST);
                     setTimeout(function () {
                         getPanelStatus(function(panelStatus) { // verify command succeeded
-                        var isArmed = panelStatus.indexOf('Armed') > -1; // true if system is armed
                         if (num === 'stay' || num === 'away') {
-                            if (isArmed) {
+                            if (isArmed(panelStatus)) {
                                 speechOutput = 'sent,' +num +',system was armed,';
                             } else {
                                 speechOutput = 'sent,' +num +',error,, system could not be armed,';
@@ -375,21 +232,6 @@ function sendCodeInSession(intent, session, callback) {
     var repromptText = "";
     var shouldEndSession = true; // end session after sending code
     var speechOutput = "";
-    var tls = require('tls');
-    var fs = require('fs');
-    var PORT = fs.readFileSync('port.txt').toString("utf-8", 0, 5);
-    var HOST = fs.readFileSync('host.txt').toString("utf-8", 0, 14);
-    var CERT = fs.readFileSync('client.crt');
-    var KEY  = fs.readFileSync('client.key');
-    var CA   = fs.readFileSync('ca.crt');
-    var options = {
-        host: HOST,
-        port: PORT,
-        cert: CERT,
-        key: KEY,
-        ca: CA,
-        rejectUnauthorized: true
-    };
     
     if (CodeSlot) {
         var num = CodeSlot.value;
@@ -402,13 +244,8 @@ function sendCodeInSession(intent, session, callback) {
                 buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
         }
         else {
-            var socket = tls.connect(options, function() {
-                console.log('sendCodeInSession socket connected to host ' +HOST);
-                if(socket.authorized){
-                    console.log('host is authorized');
-                } else {
-                    console.log('host cert auth error: ', socket.authorizationError);
-                }
+            var socket = tls.connect(socketOptions, function() {
+                console.log('sendCodeInSession socket connected to host: ' +HOST);
                 socket.write(num +'\n');
                 console.log('wrote ' +num);
             });
@@ -419,7 +256,7 @@ function sendCodeInSession(intent, session, callback) {
             });
 	
             socket.on('close', function () {
-	        console.log('sendCodeInSession socket disconnected from host ' +HOST);
+	        console.log('sendCodeInSession socket disconnected from host: ' +HOST);
                 speechOutput = "sending, " +num;
 	        callback(sessionAttributes,
                          buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
@@ -439,33 +276,166 @@ function sendCodeInSession(intent, session, callback) {
  * Sends panel status to the user and ends session.
  */
 function getStatusFromSession(panelStatus, intent, session, callback) {
-    var cardTitle = intent.name;
-    // Setting repromptText to null signifies that we do not want to reprompt the user.
-    // If the user does not respond or says something that is not understood, the session
-    // will end.
-    var repromptText = "";
-    var sessionAttributes = {};
-    var shouldEndSession = true;
-    var speechOutput = "";
+    var cardTitle = intent.name,
+        repromptText = "",
+        sessionAttributes = {},
+        shouldEndSession = true,
+        speechOutput = "";
 
-    var isReady = panelStatus.indexOf('LED Status Ready') > -1; // true if system is ready
-    var isArmed = panelStatus.indexOf('Armed') > -1; // true if system is armed
-    var hasError = panelStatus.indexOf('Error') > -1; // true if system has flagged an error condition
-    var isBypassed = panelStatus.indexOf('Bypass') > -1; // true if one or more zones are bypassed
-   
-    if (isReady) {
-       hasError ? speechOutput = 'system is ready but has an error' : speechOutput = 'system is ready'; 
-    } else {
-       var placesNotReady = findPlacesNotReady(panelStatus); // get friendly names of zones not ready;
-       speechOutput = 'these zones are not ready,' +placesNotReady;
-    }
-
-    if(isArmed) {
-       isBypassed ? speechOutput = 'system is armed and a is zone bypassed' : speechOutput = 'system is armed';
+    if (isArmed(panelStatus)) {
+        isBypassed(panelStatus) ? speechOutput = 'system is armed and bypassed' : speechOutput = 'system is armed';
+    } else if (zonesNotActive(panelStatus)) { // no zones are reporting activity or are tripped
+        hasError(panelStatus) ? speechOutput = 'system is ready but has an error' : speechOutput = 'system is ready';
+    } else { // system must not be ready
+        var placesNotReady = findPlacesNotReady(panelStatus); // get friendly names of zones not ready;
+        speechOutput = 'these zones are not ready,' +placesNotReady;
     }
 
     callback(sessionAttributes,
              buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+// --------------- global variables and commonly used functions -----------------------
+
+var tls = require('tls'),
+    fs = require('fs'),
+    PORT = fs.readFileSync('port.txt').toString("utf-8", 0, 5),
+    HOST = fs.readFileSync('host.txt').toString("utf-8", 0, 14),
+    CERT = fs.readFileSync('client.crt'),
+    KEY  = fs.readFileSync('client.key'),
+    CA   = fs.readFileSync('ca.crt');
+
+var socketOptions = {
+    host: HOST,
+    port: PORT,
+    cert: CERT,
+    key: KEY,
+    ca: CA,
+    rejectUnauthorized: true
+};
+
+/*
+ * Gets the panel status to be used in the intent handlers.
+ */
+function getPanelStatus (callback) {
+    var panelStatus = "";
+    var serverCmd = 'idle'; // send idle to server which is a noop
+
+    var socket = tls.connect(socketOptions, function() {
+        console.log('getPanelStatus socket connected to host: ' +HOST);
+        socket.write(serverCmd +'\n');
+    });
+
+    socket.on('data', function(data) {
+        panelStatus += data.toString();
+    });
+	
+    socket.on('close', function () {
+	console.log('getPanelStatus socket disconnected from host: ' +HOST);
+	callback(panelStatus);
+    });
+	
+    socket.on('error', function(ex) {
+	console.log("handled getPanelStatus socket error");
+	console.log(ex);
+    });
+}
+
+function isReady(panelStatus) {
+    return (panelStatus.indexOf('LED Status Ready') > -1) ? true : false; // true if system is ready
+}
+
+function isArmed(panelStatus) {
+    return (panelStatus.indexOf('Armed') > -1) ? true : false; // true if system is armed
+}
+
+function hasError(panelStatus) {
+    return (panelStatus.indexOf('Error') > -1) ? true : false; // true if system has flagged an error condition
+}
+
+function isBypassed(panelStatus) {
+    return (panelStatus.indexOf('Bypass') > -1) ? true : false; // // true if one or more zones are bypassed
+}
+
+/*
+ * This is a more reliable way of determining if the system is ready to be armed.
+ * isReady() can also be used but is prone to falsing because the motion sensors cause the system
+ * to momentarly report a not ready condition which causes problems later on when finding out which zones aren't ready.
+ *
+*/
+function zonesNotActive(panelStatus) {
+    var zoneRegex = /(Zone\d \d{1,2})((, \d{1,2}){1,8})?/g; // find zones with numbers, indicating activity
+    var zonesActive = panelStatus.match(zoneRegex); // array with active zones
+    return (zonesActive === null) ? true : false; // true if zones report no activity
+}
+
+/*
+ * Mapping from alarm zones to friendly place names.
+ */
+function zoneToPlace(zone, sensor) {
+    var places = {
+        "Zone1": {
+            "1":  "front door",
+	    "2":  "living room left window",
+	    "3":  "living room right window",
+	    "4":  "dining room left window",
+	    "5":  "dining room center left window",
+	    "6":  "dining room center right window",
+	    "7":  "dining room right window",
+	    "8":  "kitchen left window"
+        },
+        "Zone2": {
+            "1":  "kitchen right window",
+	    "2":  "breakfast nook left window",
+	    "3":  "breakfast nook side right window",
+	    "4":  "breakfast nook left window",
+	    "5":  "breakfast nook center left window",
+	    "6":  "breakfast nook center right window",
+	    "7":  "breakfast nook right window",
+	   "8":  "family room slider door"
+        },
+        "Zone3": {
+            "1":  "family room left window",
+	    "2":  "family room center window",
+	    "3":  "family room right window",
+            "4":  "guest bedroom left window",
+	    "5":  "guest bedroom center left window",
+	    "6":  "guest bedroom center right window",
+	    "7":  "guest bedroom right window",
+	    "8":  "first floor bathroom window"
+        },
+        "Zone4": {
+            "1":  "front landing window",
+	    "2":  "upstairs slider",
+	    "3":  "front motion",
+	    "4":  "hall motion",
+	    "5":  "upstairs motion",
+	    "6":  "playroom motion",
+	    "7":  "playroom window",
+	    "8":  "playroom door"
+        }
+    };
+    return places[zone][sensor];
+}
+
+/*
+ * Find friendly names of places not ready to be armed.
+ * Assumes at least one zone is not ready. Will fail otherwise. 
+ */
+function findPlacesNotReady(panelStatus) {
+    var zoneRegex = /(Zone\d \d{1,2})((, \d{1,2}){1,8})?/g;
+    var zonesNotReady = panelStatus.match(zoneRegex); // array with zones not ready
+    var placesNotReady = "";
+    for(i = 0; i < zonesNotReady.length; i++) {
+        var zoneNotReady = zonesNotReady[i].slice(0,5);
+        var sensorRegex = /\d{1,2}/g;
+        var sensorsNotReady = zonesNotReady[i].slice(6).match(sensorRegex); // array with sensors in zone not ready
+        for(j = 0; j < sensorsNotReady.length; j++) {
+            var sensorNotReady = sensorsNotReady[j];
+	    placesNotReady += zoneToPlace(zoneNotReady, sensorNotReady) +',';
+        }
+    }
+    return placesNotReady;
 }
 
 function createNumberAttributes(key) {
