56b1a51c9cf282bd93cec471b9a93e94ef58e05e
goruck <lindo.st.angel@gmail.com>
2016-06-19 17:29:45 -0700

    added time as a feature to the model

--- R/predknn.R
+++ R/predknn.R
@@ -6,26 +6,36 @@ cat("********** New R Run **********\n")
 TEMPORAL_CUTOFF <- -120 # 120 seconds ago
 TEMPORAL_VALUE  <- -9999 # a long time ago
 library(class) # for knn
+library(lubridate) # for date / time conversions
+### function to convert time stamp from UTC to local time and extract hour
+extractHour <- function(dateTime) {
+  td <- parse_date_time(dateTime, orders="ymd hms", tz="UTC")
+  td_local <- with_tz(td, tz="America/Los_Angeles")
+  h <- hour(td_local)
+  return(h)
+}
+### function to limit values in the dataframe
+limitNum <- function(num) {
+  if (num < TEMPORAL_CUTOFF) num <- TEMPORAL_VALUE
+  return(num)
+}
 
-### get zone data from R's arguements and make it relative to observation times
+### get zone data from R's arguements and condition it
 args = commandArgs(trailingOnly=TRUE)
-obsTime <- as.integer(args[1])
-#obsTime
-zoneTimes <- lapply(strsplit(args[2], ","), as.numeric)[[1]]
-#zoneTimes
-zoneRelTimes <- zoneTimes - obsTime
-#zoneRelTimes
+ts <- args[1] # observation timestamp in UTC format
+hr <- extractHour(ts)
+obsTime <- as.integer(args[2]) # observation time in seconds (derived from linux system time)
+zoneTimes <- lapply(strsplit(args[3], ","), as.numeric)[[1]] # abs zone act/deact times
+zoneRelTimes <- zoneTimes - obsTime # calulate relative zone act/deact times
 #zoneRelActTimes <- zoneRelTimes[1:32]
-#zoneRelActTimes
 #zoneRelDeActTimes <- zoneRelTimes[33:64]
-#zoneRelDeActTimes
 
 ### put observations into a data frame and add column labels
-#oldw <- getOption("warn")
-#options(warn = -1) # supress warnings in case not all zones have data
-df <- data.frame(matrix(zoneRelTimes, nrow = 1, ncol = 64))
-#options(warn = oldw) # turn back on warnings
-colnames(df) <- c("za1","za2","za3","za4","za5","za6","za7","za8",
+oldw <- getOption("warn")
+options(warn = -1) # supress warnings in case not all zones have data
+df <- data.frame(matrix(c(hr, zoneRelTimes), nrow = 1, ncol = 66))
+options(warn = oldw) # turn back on warnings
+colnames(df) <- c("clock","za1","za2","za3","za4","za5","za6","za7","za8",
                   "za9","za10","za11","za12","za13","za14","za15","za16",
                   "za17","za18","za19","za20","za21","za22","za23","za24",
                   "za25","za26","za27","za28","za29","za30","za31","za32",
@@ -41,33 +51,60 @@ zaKeep = c("za1","za16","za27","za28","za29","za30","za32")
 zdKeep = NULL
 
 ### select test data set, apply temporal filter, print observation
-keep = c(zaKeep, zdKeep)
-testData = df[keep]
-testData[testData < TEMPORAL_CUTOFF] <- TEMPORAL_VALUE
-print(testData, row.names = FALSE)
+keep <- c(zaKeep, zdKeep)
+rawTestData <- df[keep]
+rawTestData[rawTestData < TEMPORAL_CUTOFF] <- TEMPORAL_VALUE
+rawTestData <- merge(df["clock"], rawTestData)
+print(rawTestData, row.names = FALSE)
 
 ### load training data and calculate k
 df = read.csv("/home/pi/all/R/testFromSimpledb.csv")
 #df = read.csv("/home/pi/all/R/knnTrain.csv")
 #df = read.csv("/home/pi/dev/knnTrainMix.csv")
-k <- ceiling(sqrt(nrow(df)))
-cat("k: ", k, "\n")
+k <- floor(sqrt(nrow(df)))
 #k <- 13 # generally a good default value for current data set
+cat("k: ", k, "\n")
 
 ### define function to make a prediction for a specific pattern
 predictPattern <- function(zaKeep, zdKeep, pattern, df, k) {
 
-  ### condition training data for pattern, apply temporal filter
-  keep <- c(zaKeep, zdKeep, pattern)
-  dfKeep <- df[sample(nrow(df)), keep]
+  ### apply filters to data and randomize rows
+  keep <- c("clock", zaKeep, zdKeep, pattern)
+  dfKeep <- df[sample(nrow(df)), keep] # randomize
+
   #dfKeepTrue <- dfKeep[dfKeep$pattern == TRUE, ]
   #x <- nrow(dfKeepTrue)
   #y <- dfKeep[dfKeep$pattern == FALSE, ]
   #dfKeepFalse <- y[1:x, ]
   #dfKeep <- rbind(dfKeepTrue, dfKeepFalse)
-  trainData <- dfKeep[, 1:(ncol(dfKeep)-1)]
-  trainData[trainData < TEMPORAL_CUTOFF] <- TEMPORAL_VALUE
-  trainLabels <- dfKeep[, ncol(dfKeep)]
+
+  ### replace date / time stamps with only observation hour in local time
+  dfKeep["clock"] <- lapply(dfKeep["clock"], extractHour)
+  
+  ### apply the limit function to all elements except the clock and pattern columns
+  drops <- c("clocks", pattern)
+  dfKeep[, !(names(dfKeep) %in% drops)] <- apply(dfKeep[, !(names(dfKeep) %in% drops)], c(1, 2), limitNum)
+
+  ### merge the training and test data
+  rawTestData[9] <- 0 # create a dummy col to make rbind work ok
+  colnames(rawTestData)[9] <- pattern
+  dfKeep <- rbind(dfKeep, rawTestData)
+
+  ### scale the  combined data to have col mean = 0 and sd = 1
+  drops <- pattern
+  dfKeep[, !(names(dfKeep) %in% drops)] <- scale(dfKeep[, !(names(dfKeep) %in% drops)])
+
+  ### form training and test data sets
+  trainData = dfKeep[1:(nrow(dfKeep) - 1), 2:(ncol(dfKeep) - 1)] # 2 = do not include time
+  testData = dfKeep[nrow(dfKeep), 2:(ncol(dfKeep) - 1)] # 2 = do not include time
+
+  ### form training and test labels
+  trainLabels = dfKeep[1:(nrow(dfKeep) - 1), ncol(dfKeep)]
+  #testLabels = dfKeep[nrow(dfKeep), ncol(dfKeep)] # not required in normal mode
+
+  #trainData <- dfKeep[, 1:(ncol(dfKeep)-1)]
+  #trainData[trainData < TEMPORAL_CUTOFF] <- TEMPORAL_VALUE
+  #trainLabels <- dfKeep[, ncol(dfKeep)]
 
   ### make prediction for pattern
   #cat("making prediction for pattern: ", pattern, "\n")
