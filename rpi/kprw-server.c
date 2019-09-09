72b381be20f408de1a4187fe5631fb4be79aa77f
goruck <lindo.st.angel@gmail.com>
2019-09-08 17:26:10 -0700

    use short int for zone index to prevent overflow warn

--- rpi/kprw-server.c
+++ rpi/kprw-server.c
@@ -424,7 +424,8 @@ static inline int popElement2(char *element, int num) {
 
 // Decode bits from panel into commands and messages.
 static int decode(char * word, char * msg, int * allZones) {
-  int cmd = 0, zones = 0, button = 0, i = 0;
+  int cmd = 0, zones = 0, button = 0;
+  unsigned short i = 0;
   char year3[2],year4[2],month[2],day[2],hour[2],minute[2],str[2];
 
   cmd = getBinaryData(word,0,8);
